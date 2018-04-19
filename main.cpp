72e8886970655fb0a80d31cff6ff186e7ada99f9
Ludmila <Lmartakova@luxoft.com>
2018-04-19 12:44:44 +0300

    b1 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    qDebug() << "Hello world!";
+
+    qDebug() << "Hello world!b1";
+
 
     return a.exec();
 }
